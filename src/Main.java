666cb42559e62fdd6a2c9de85d87548ab0c2a366
GeorgeM22 <georgemichaelinfo@gmail.com>
2023-04-07 18:04:07 -0400

    cat feature has been added

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Practicing Git");
+        Cat cat = new Cat();
+        cat.pet="cat";
+        cat.price="100";
+        cat.color="black";
     }
 }
