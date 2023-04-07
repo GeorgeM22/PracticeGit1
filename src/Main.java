1933b5c87f45fd9b90b4f63dfcdd18744fe89466
GeorgeM22 <georgemichaelinfo@gmail.com>
2023-04-07 18:19:29 -0400

    dog feature added

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Practicing Git");
+        Dog dog = new Dog();
+        dog.name="Pepo";
+        dog.color="white";
+        dog.gender="male";
     }
 }
